bfbf3e9cfcf3cca30be48f9c71de1a55787739e0
Mille Elfver <mille.elfver98@gmail.com>
2024-04-17 20:08:23 +0200

    Added template

--- BlogWeb/obj/Debug/net8.0/BlogWeb.AssemblyInfo.cs
+++ BlogWeb/obj/Debug/net8.0/BlogWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45214c1eb1ac8560415b8a28002dffdf85195583")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2829bbeed6b20054578a57c4f35effacbca758ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
